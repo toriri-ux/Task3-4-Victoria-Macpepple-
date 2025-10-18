7f8e27c1c419744db7edde8a98136ae026b07f6c
Tori <Vtmacp@gmail.com>
2025-10-18 20:08:24 +0100

    updated

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,5 @@
     public class Main {
         public static void main(String[] args) {
 System.out.println("Hello Vistula");
-
-                float weightOnEarthMacpepple = 55;
-                double marsGravity = 0.38;
-                double weightOnMars = weightOnEarthMacpepple * marsGravity;
-System.out.println("your weight on mars is : " + weightOnMars +"kg");
-
-
         }
     }
