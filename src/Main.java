b9d6e7dc33e3cb6b949c7dbf77a2da85b408c12f
Tori <Vtmacp@gmail.com>
2025-10-18 19:43:55 +0100

    updated weight on mars task

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@
     public class Main {
         public static void main(String[] args) {
 System.out.println("Hello Vistula");
+
+                float weightOnEarthMacpepple = 55;
+                double marsGravity = 0.38;
+                double weightOnMars = weightOnEarthMacpepple * marsGravity;
+System.out.println("your weight on mars is : " + weightOnMars +"kg");
+
+
         }
     }
